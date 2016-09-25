e485095a31b0c57b9bda8f0fd4af2533688720b8
Sohan Murthy <sohan@amino.com>
2016-09-25 11:20:40 -0700

    adds more lines

--- sketch.js
+++ sketch.js
@@ -1,11 +1,24 @@
 function setup() {
-  createCanvas(480, 120);
+  createCanvas(window.innerWidth, window.innerHeight);
   strokeWeight(8);
 }
 
 function draw() {
   background(204);
-  for (var i = 20; i < 400; i += 60) {
-    line( i, 40, i + 60, 20);
+  for (var i = 20; i < window.innerWidth; i += 60) {
+    line( i, 80, i + 60, 20);
+  }
+  for (var i = 20; i < window.innerWidth; i += 60) {
+    line( i, 160, i + 60, 100);
+  }
+  for (var i = 20; i < window.innerWidth; i += 60) {
+    line( i, 240, i + 60, 180);
   }
 }
+
+// function draw() {
+//   background(204);
+//   for (var i = 20; i < window.innerWidth; i += 60) {
+//     line( i, 160, i + 60, 100);
+//   }
+// }
