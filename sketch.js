f9b5024d65f83fdedf2593c63f9901125201ab19
Sohan Murthy <sohan@amino.com>
2016-09-25 14:57:49 -0700

    adds cross hatches and color animation

--- sketch.js
+++ sketch.js
@@ -1,16 +1,32 @@
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
-  strokeWeight(3);
+  background(220);
+  strokeWeight(6);
+  frameRate(8);
 }
 
 function draw() {
-  background(220);
-  stroke(0,204,153);
+//bottom-to-top
   for (var x = 20; x < width-30; x += 60) {
     for (var y = 80; y < height-20; y += 80) {
+      var r = random(0,50);
+      var g = random(200,235);
+      var b = random(100,200);
+    stroke(r,g,b);
     line( x, y, x + 60, y - 60);
     }
   }
+//top-to-bottom
+  for (var x = 20; x < width-30; x += 60) {
+    for (var y = 80; y < height-20; y += 80) {
+      var r = random(0,50);
+      var g = random(200,235);
+      var b = random(100,200);
+    stroke(r,g,b);
+    line( x, y - 60, x + 60, y);
+    }
+  }
+
 }
 
 // function draw() {
